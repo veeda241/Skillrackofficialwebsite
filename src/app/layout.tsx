d65541a0137f854937aa8052f45c82499360ec1f
Vyas Senthilkumar <vyas.sk17@gmail.com>
2025-08-04 14:21:17 +0000

    Hydration failed because the server rendered HTML didn't match the clien

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         {children}
         <Toaster />
       </body>
